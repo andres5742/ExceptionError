f8080fc92949083edc4c0fd82454d84f8fb33d88
cristian andres sanchez <perezandres574@gmail.com>
2025-03-18 22:49:42 -0500

    exception handling (404 not found)

--- src/main/java/com/andres/springboot/exception/springboot_exception/controllers/HandlerExceptionController.java
+++ src/main/java/com/andres/springboot/exception/springboot_exception/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.andres.springboot.exception.springboot_exception.models.Error;
 
@@ -20,6 +21,18 @@ public class HandlerExceptionController {
         error.setMessage(ex.getMessage());
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return ResponseEntity.internalServerError().body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
+
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("api Rest no encontrado");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
 
 }
